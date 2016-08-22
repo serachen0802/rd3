551357c8a6f99d9c59d2c00e85a7511dbab7fda9
sera <j62988g@yahoo.com.tw>
2016-08-22 08:45:03 +0000

    Landmine2

---
+++ Landmine2.php
@@ -0,0 +1,62 @@
+<?php
+$time1 = microtime(true);
+$maxBomb = 1200;
+for($x=0; $x<60; $x++){
+    for($y=0; $y<50; $y++){
+        $arr[$x][$y]='0';
+    }
+}
+
+for($setBomb = 0; $setBomb < $maxBomb; $setBomb++) {
+    while(true) {
+        // 隨機產生一個炸彈
+        $row = rand(0, 59);
+        $column = rand(0, 49);
+        if ($arr[$row][$column] != 'M') {
+            $arr[$row][$column] = 'M';
+            break;
+        }
+    }
+}
+
+for($x = 0; $x < 10; $x++){
+    for($y = 0; $y < 10; $y++){
+        if($arr[$x][$y]!= 'M'){
+            if ($arr[$x+1][$y] == 'M') {
+                $arr[$x][$y]++;
+            }
+            if ($arr[$x-1][$y] == 'M') {
+                $arr[$x][$y]++;
+            }
+            if ($arr[$x][$y+1] == 'M') {
+                $arr[$x][$y]++;
+            }
+            if ($arr[$x][$y-1] == 'M') {
+                $arr[$x][$y]++;
+            }
+            if ($arr[$x+1][$y+1] == 'M') {
+                $arr[$x][$y]++;
+            }
+            if ($arr[$x-1][$y-1] == 'M') {
+                $arr[$x][$y]++;
+            }
+            if ($arr[$x-1][$y+1] == 'M') {
+                $arr[$x][$y]++;
+            }
+            if ($arr[$x+1][$y-1] == 'M') {
+                $arr[$x][$y]++;
+            }
+        }
+    }
+}
+
+foreach($arr as $value) {
+    foreach ($value as $val) {
+        $result .= $val;
+    }
+    $result .= "N";
+}
+//  echo substr_replace($result,'',-1);
+$time2 = microtime(true);
+
+echo ($time2-$time1);
