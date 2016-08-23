660b0329b839cce32a15b7cfbd5b9b07dab4a120
sera <j62988g@yahoo.com.tw>
2016-08-23 04:26:01 +0000

    CheckLandmine

--- CheckLandmine.php
+++ CheckLandmine.php
@@ -1,8 +1,7 @@
 <?php
-
 header("content-type: text/html; charset=utf-8");
-$map = $_GET['map'];
 
+$map = $_GET['map'];
 $notN = substr($map, -1);
 if ($notN == 'N'){
     echo "不符合。因為最後一個字元不可以是N<br>";
@@ -20,9 +19,7 @@ for ($i = 0; $i < 10; $i++) {
     $checkNum2 = preg_split('//', $checkNum[$i], -1, PREG_SPLIT_NO_EMPTY);
     $arr[$i] = $checkNum2;
 }
-
 $arr2=[$arr];
-
 for($x = 0; $x < 10; $x++){
     for($y = 0; $y < 10; $y++){
         if($arr2[0][$x][$y] != "M"){
@@ -30,6 +27,7 @@ for($x = 0; $x < 10; $x++){
         }
     }
 }
+
 for($x = 0; $x < 10; $x++){
     for($y = 0; $y < 10; $y++){
         if($arr2[0][$x][$y]!= 'M'){
@@ -58,13 +56,16 @@ for($x = 0; $x < 10; $x++){
                 $arr2[0][$x][$y]++;
             }
         }
+        if ($arr[$x][$y] != $arr2[0][$x][$y]) {
+            echo "不符合。(".$x.",".$y.")數字有誤";
+        }
     }
 }
 
-if($arr != $arr2[0]){
-    echo "不符合。數字有誤";
-}else{
-    echo "符合。";
-}
+// if ($arr != $arr2[0]) {
+//     echo "不符合。數字有誤";
+// } else {
+//     echo "符合。";
+// }
 // print_r($arr2);
 // print_r($arr);
