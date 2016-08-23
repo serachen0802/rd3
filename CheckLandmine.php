bd149144e9ea730fe76710b70a9e074d6ba32c04
sera <j62988g@yahoo.com.tw>
2016-08-23 04:28:07 +0000

    CheckLandmine

--- CheckLandmine.php
+++ CheckLandmine.php
@@ -1,12 +1,14 @@
 <?php
 header("content-type: text/html; charset=utf-8");
 
+// 最後一個字元不可以為N
 $map = $_GET['map'];
 $notN = substr($map, -1);
 if ($notN == 'N'){
     echo "不符合。因為最後一個字元不可以是N<br>";
 }
 
+// 炸彈的數量需為40
 $countM = substr_count($map,"M");
 if ($countM > 40) {
     echo "不符合。因為炸彈數量大於40<br>";
@@ -19,6 +21,8 @@ for ($i = 0; $i < 10; $i++) {
     $checkNum2 = preg_split('//', $checkNum[$i], -1, PREG_SPLIT_NO_EMPTY);
     $arr[$i] = $checkNum2;
 }
+
+// 將陣列不為M的位置改為0
 $arr2=[$arr];
 for($x = 0; $x < 10; $x++){
     for($y = 0; $y < 10; $y++){
@@ -61,11 +65,3 @@ for($x = 0; $x < 10; $x++){
         }
     }
 }
-
-// if ($arr != $arr2[0]) {
-//     echo "不符合。數字有誤";
-// } else {
-//     echo "符合。";
-// }
-// print_r($arr2);
-// print_r($arr);
